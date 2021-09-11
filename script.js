a785551bf221ea6b974dbfdaa79d5139b56941f2
kru01 <huuphuc091203@gmail.com>
2021-09-12 00:12:52 +0700

    super buggy fill mode
    -will try to fix soon

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const gridContainer = document.getElementById('gridContainer');
 const gridSize = document.getElementById('gridSize');
 const mode = document.getElementById('modePicker');
 
+// These will be defined in createGrid()
+let gridItems; // gridItems = document.querySelectorAll(`.gridItems`);
+let gridArray, gridMatrix; // gridArray = Array.from(gridItems), gridMatrix = arrToMatrix(gridArray, currentDimension);
+
 const rainbow = document.getElementById('rainbow');
+const fill = document.getElementById('fill');
+
 const eraser = document.getElementById('eraser');
+const darken = document.getElementById('darken');
+
 const gridLines = document.getElementById('gridLines');
 const resetGrid = document.getElementById('resetGrid');
 
@@ -16,16 +24,21 @@ window.onload = () => {
     mouseOverColor();
 }
 
-canvasUpdate();
-penUpdate();
-mode.addEventListener('click', drawMode);
+starter();
 
-rainbow.addEventListener('click', rainbowToggle);
-
-eraser.addEventListener(`click`, eraserToggle);
-gridLines.addEventListener(`click`, gridLinesToggle);
-updateGrid();
-resetGrid.addEventListener('click', clearGrid);
+function starter() {
+    canvasUpdate();
+    penUpdate();
+    mode.addEventListener('click', drawMode);
+    
+    rainbow.addEventListener('click', rainbowToggle);
+    fill.addEventListener('click', fillMode);
+    
+    eraser.addEventListener(`click`, eraserToggle);
+    gridLines.addEventListener(`click`, gridLinesToggle);
+    updateGrid();
+    resetGrid.addEventListener('click', clearGrid);
+}
 
 /* Everything on left tool bar #settings */
 
@@ -61,23 +74,118 @@ function rainbowToggle() {
         activate("rainbow");
         penCurrentColor = randomColor();
     }
+} 
+// for rainbow on mouseHold mode to work, randomColor() is also called in mouseHoldColor() and removeMouseHold()
+function randomColor() {
+    // const randomH = Math.floor(Math.random() * 360); 
+    // const randomS = Math.floor(Math.random() * 100);
+    // const randomL = Math.floor(Math.random() * 100);
+    
+    // return `hsl(${randomH}, ${randomS}%, ${randomL}%)`;
+
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    return `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
-function randomColor() {
-    const randomR = Math.floor(Math.random() * 256)
-    const randomG = Math.floor(Math.random() * 256)
-    const randomB = Math.floor(Math.random() * 256)
-    return `rgb(${randomR}, ${randomG}, ${randomB})`
+/* FILL MODE */
+function fillMode() {
+    if(fill.classList.contains(`active`)) {
+        penCurrentColor = `${penUpdate()}`;
+        deactivate("fill");
+        drawMode();
+    } else {
+        const options = document.querySelectorAll(`.options`);
+
+        options.forEach(option => {
+            option.classList.remove(`active`);
+        });
+
+        fill.classList.add(`active`);
+        fillActive();
+    }
+}
+
+function fillActive() {
+    removeMouseHold();
+    removeMouseOver();
+
+    let itemClickedIndex;
+    const gridItems = document.querySelectorAll(`.gridItems`);
+
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('click', (e) => {
+            itemClickedIndex = gridArray.indexOf(e.target);
+
+            let matrixRow = Math.floor(itemClickedIndex / currentDimension);
+            let matrixCol = itemClickedIndex % currentDimension;
+
+            floodFill(matrixRow, matrixCol);
+        });
+    });
+}
+
+function floodFill(row, col) {
+    const fillColor = penCurrentColor;
+    const nodes = [{row, col}];
+    const seedColor = canvasCurrentColor;
+
+    while(nodes.length > 0) {
+        const {row: r, col: c} = nodes.pop();
+
+        if(r < 0 || r > gridMatrix.length - 1) continue;
+
+        if(c < 0 || c > gridMatrix[r].length - 1) continue;
+        
+        const node = gridMatrix[r][c];
+
+        if(node.style.background === fillColor) continue;
+        if(node.style.background !== seedColor) continue;
+
+        node.style.background = fillColor;
+
+        nodes.push({row: r + 1, col: c});
+        nodes.push({row: r - 1, col: c});
+        nodes.push({row: r, col: c - 1});
+        nodes.push({row: r, col: c + 1});
+    }
+}
+
+function arrToMatrix(arr, elementsPerSubArray) {
+    const matrix = [];
+    let rows = -1;
+
+    for(let arrIndex = 0; arrIndex < arr.length; arrIndex++) {
+        if (arrIndex % elementsPerSubArray === 0) {
+            rows++;
+            matrix[rows] = [];
+        }
+
+        matrix[rows].push(arr[arrIndex]);
+    }
+
+    return matrix;
 }
 
 /* ERASER */
 function eraserToggle() {
     if(eraser.classList.contains(`active`)) {
-        penCurrentColor = penUpdate();
+        penCurrentColor = `${penUpdate()}`;
         deactivate("eraser");
     } else {
         activate("eraser");
         penCurrentColor = canvasCurrentColor;
+    }
+}
+
+/* DARKEN */
+function darkenToggle() {
+    if(darken.classList.contains(`active`)) {
+        penCurrentColor = `${penUpdate()}`;
+        deactivate("darken");
+    } else {
+        activate("darken");
     }
 }
 
@@ -109,9 +217,12 @@ function createGrid(dimension) {
     gridContainer.style.gridTemplate = `repeat(${dimension}, 1fr) / repeat(${dimension}, 1fr)`;
     
     for(let i = 0; i < dimension * dimension; i++) {
-        const gridItems = document.createElement('div');
-        gridItems.classList.add('gridItems');
-        gridContainer.appendChild(gridItems);
+        const itemsInit = document.createElement('div');
+        itemsInit.classList.add('gridItems');
+        gridContainer.appendChild(itemsInit);
+        gridItems = document.querySelectorAll(`.gridItems`);
+        gridArray = Array.from(gridItems);
+        gridMatrix = arrToMatrix(gridArray, currentDimension);
     }
 }
 
@@ -121,8 +232,6 @@ function deleteGrid() {
 
 /* Toogle GRID LINES */
 function gridLinesToggle() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     if(gridLines.classList.contains(`active`)) {
         gridItems.forEach(gridItem => {
             gridItem.style.border = `none`;
@@ -132,11 +241,14 @@ function gridLinesToggle() {
     } else {
         gridLinesActive(currentDimension);
         activate("gridLines");
+        if(fill.classList.contains(`active`)) {
+            removeMouseOver();
+            removeMouseHold();
+        }
     }
 }
 
 function gridLinesActive(dimension) {
-    const gridItems = document.querySelectorAll('.gridItems');
     const maxItemsNum = dimension * dimension;
 
     gridItems.forEach(gridItem => {
@@ -145,7 +257,6 @@ function gridLinesActive(dimension) {
     });
 
     for(let i = dimension - 1; i < maxItemsNum; i += dimension) {
-        console.log(i);
         gridItems[i].style.borderRight = `1px solid black`;
     }
 
@@ -156,8 +267,6 @@ function gridLinesActive(dimension) {
 
 /* RESET */
 function clearGrid() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     gridItems.forEach(gridItem => {
         gridItem.style.background = canvasCurrentColor;
     });
@@ -202,24 +311,18 @@ function drawMode() {
 }
 
 function mouseOverColor() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     gridItems.forEach(gridItem => {
         gridItem.addEventListener("mouseover", changeColor);
     });
 }
 
 function removeMouseOver() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     gridItems.forEach(gridItem => {
         gridItem.removeEventListener("mouseover", changeColor);
     });
 }
 
 function mouseHoldColor() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     gridItems.forEach(gridItem => {
         gridItem.addEventListener("mousedown", changeColor);
         gridItem.addEventListener("mouseenter", (e) => {
@@ -233,8 +336,6 @@ function mouseHoldColor() {
 }
 
 function removeMouseHold() {
-    const gridItems = document.querySelectorAll('.gridItems');
-
     gridItems.forEach(gridItem => {
         gridItem.removeEventListener("mousedown", changeColor);
         gridItem.removeEventListener("mouseenter", (e) => {
